f09bde7cb6741c9d52c87c1262ff36ebb87314b6
lalitadityamv <lalitadityamv@gmail.com>
2025-10-19 17:39:29 +0530

    version-1

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+const navLinks=[
+                    {label:"store"},
+                    {label:"Mac"},
+                    {label:"iPhone"},
+                    {label:"vision"},
+                    {label:"AirPods"},
+                ]
